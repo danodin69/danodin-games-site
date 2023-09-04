7cabde3e359f99a1e60323de0e9a786dd7e30132
dpoxo <danphilodin@gmail.com>
2023-09-04 19:35:32 +0100

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do 
   get 'games', to:"games#index", as: :games
   get 'contact' ,to: "contact#index"
-  get 'about/index'
+  get 'about', to: "about#index"
 
   get 'privacy_policy',to:'privacy_policy#index'
   
