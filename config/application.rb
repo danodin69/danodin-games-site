ec5b4a59e7cd6d121ef7da34ea6cd739ba8933f0
dpoxo <danphilodin@gmail.com>
2023-09-07 01:26:32 +0100

    font fixes

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module DanodinGames
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.assets.paths << "#{Rails.root}/app/assets/videos"
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
