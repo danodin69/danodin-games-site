fa0f39a1ac9110ae4d0025c62e3861b05ca82834
Dan Odin ʕ•ᴥ•ʔ <56373611+danodin69@users.noreply.github.com>
2025-04-26 12:49:38 +0100

    bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << ENV['DEVELOPMENT_HOSTS']
+  config.host_authorization = { exclude: ->(request) { true } }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
